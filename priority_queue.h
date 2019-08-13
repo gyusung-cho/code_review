55e48ce5de5133c817d60ff226199453b3034795
gyusung-cho <42734059+gyusung-cho@users.noreply.github.com>
2019-08-13 15:32:27 +0900

    apply the code reviewer's guide

--- priority_queue.h
+++ priority_queue.h
@@ -1,15 +1,31 @@
+#ifndef PRIORITY_QUEUE_H_
+#define PRIORITY_QUEUE_H_
+
 #include <queue>
 
+/* 
+  TemplatePriorityQueue : Priority Queue Implementation Class
+  members :
+  empty(): check if it is empty
+  top(): return top item
+  size(): return the size of queue
+  push(): add new item
+  pop(): delete a top item
+*/
+
 template <typename T> 
-class templatePriorityQueue {
-	private:
-		std::priority_queue<T> storage;
-	public:
-		templatePriorityQueue();
-		~templatePriorityQueue();
-		bool empty() const;
-		const T& top() const;
-		int size();
-		void push(const T&);
-		void pop();	
+class TemplatePriorityQueue {
+ public:
+  TemplatePriorityQueue();
+  ~TemplatePriorityQueue();
+  bool empty() const;
+  const T& top() const;
+  int size();
+  void push(const T&);
+  void pop();	
+
+ private:
+  std::priority_queue<T> storage;
 };
+
+#endif // PRIORITY_QUEUE_H_
